1b4cf3b4ab689fb736b4241575c898d419df0d83
HP <HP@DUMAN>
2025-10-21 00:58:51 +0300

    Proje dosyası ekle.

---
+++ UcakRezervasyon.Core/User.cs
@@ -0,0 +1,17 @@
+﻿namespace UcakRezervasyon.Core
+{
+    public class User
+    {
+        private global::System.String passwordHash;
+        private global::System.String userName;
+        private global::System.String surname;
+        private global::System.String name;
+
+        public int Id { get; set; }
+        public string UserName { get => userName; set => userName = value; }
+        public string PasswordHash { get => passwordHash; set => passwordHash = value; }
+        public string Name { get => name; set => name = value; }
+        public string Surname { get => surname; set => surname = value; }
+
+    }
+}
---
+++ UcakRezervasyon/Program.cs
@@ -0,0 +1,16 @@
+﻿using UcakRezervasyon.Core;
+
+Console.WriteLine("Test Başladı!");
+
+User yenikullanici = new User
+{
+    Id = 1,
+    UserName = "testuser",
+    PasswordHash = "hashedpassword",
+    Name = "Test",
+    Surname = "Kullanıcı"
+};
+
+Console.WriteLine($"yeni kullanici id:{yenikullanici.Id},username: {yenikullanici.UserName},name:{yenikullanici.Name}");
+
+Console.ReadLine();
